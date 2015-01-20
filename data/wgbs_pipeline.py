e80c93bd0412aa83a2853bee4f9298307d01df73
nsheff <nathan@code.databio.org>
2015-01-20 14:27:18 +0100

    Update annotation with new columns.

---
+++ data/wgbs_pipeline.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.7
+import os
+import platform
+
+print("hello world.")
+
+print(platform.python_version())
+
+#lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+
+def somefunction():
+    pass
+
