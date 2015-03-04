032f87e795c15ccd4655d8902e18b882c27774e0
jklughammer <jklughammer@cemm.oeaw.ac.at>
2015-03-04 21:40:11 +0100

    add merge preparation and submission of rnaBitSeq and rnaTopHat to project_sample_loop

--- src_pipeline/ngstk.py
+++ src_pipeline/ngstk.py
@@ -1,72 +1,79 @@
-
 import pipetk
 import os
 import subprocess
 # Path variables
-########################################################################################
+# #######################################################################################
 # in here is stuff that will be used by multiple pipelines.
 
 def bam_to_fastq(bam_file, out_fastq_pre, paired_end, paths, sanity_check=True):
-	pipetk.timestamp("### Fastq conversion: ")
-	pipetk.make_sure_path_exists(os.path.dirname(out_fastq_pre))
-		# Build commands:
+    pipetk.timestamp("### Fastq conversion: ")
+    pipetk.make_sure_path_exists(os.path.dirname(out_fastq_pre))
+    # Build commands:
 
-	cmd = "java -jar "
-	cmd += os.path.join(paths.picard_dir, "SamToFastq.jar")
-	cmd += " I=" + bam_file
-	cmd += " F=" + out_fastq_pre + "_R1.fastq"
+    cmd = "java -jar "
+    cmd += os.path.join(paths.picard_dir, "SamToFastq.jar")
+    cmd += " I=" + bam_file
+    cmd += " F=" + out_fastq_pre + "_R1.fastq"
 
-	if paired_end:
-		cmd += " F2=" + out_fastq_pre + "_R2.fastq"
+    if paired_end:
+        cmd += " F2=" + out_fastq_pre + "_R2.fastq"
 
-	cmd += " INCLUDE_NON_PF_READS=true"
+    cmd += " INCLUDE_NON_PF_READS=true"
 
-	pipetk.call_lock(cmd, "lock.fastq", paths.pipeline_outfolder, out_fastq_pre + "_R1.fastq")
+    pipetk.call_lock(cmd, "lock.fastq", paths.pipeline_outfolder, out_fastq_pre + "_R1.fastq")
 
-	# Sanity checks:
-	if (sanity_check):
-		bam_size = count_reads(bam_file)
-		pipetk.report_result("Bam reads", str(bam_size), paths)
-		fastq_size = subprocess.check_output("wc -l " + out_fastq_pre + "_R1.fastq | cut -f1 -d' '", shell=True)
-		if not paired_end:
-			fastq_reads = int(fastq_size) / 4
-		else:
-			fastq_reads = int(fastq_size) / 2
-		pipetk.report_result("Fastq reads" , fastq_reads, paths)
-		if (fastq_reads!= int(bam_size)):
-			raise Exception("Fastq conversion error? Size doesn't match unaligned bam")
-
+    # Sanity checks:
+    if (sanity_check):
+        bam_size = count_reads(bam_file)
+        pipetk.report_result("Bam reads", str(bam_size), paths)
+        fastq_size = subprocess.check_output("wc -l " + out_fastq_pre + "_R1.fastq | cut -f1 -d' '", shell=True)
+        if not paired_end:
+            fastq_reads = int(fastq_size) / 4
+        else:
+            fastq_reads = int(fastq_size) / 2
+        pipetk.report_result("Fastq reads", fastq_reads, paths)
+        if (fastq_reads != int(bam_size)):
+            raise Exception("Fastq conversion error? Size doesn't match unaligned bam")
 
 
 def count_lines(file):
-	x = subprocess.check_output("wc -l " + file + " | cut -f1 -d' '", shell=True)
-	return x
+    x = subprocess.check_output("wc -l " + file + " | cut -f1 -d' '", shell=True)
+    return x
 
 
-def count_reads_bam(file , param):
-	x = subprocess.check_output("samtools view -c " + param + file, shell=True)
-	return x
+def count_reads_bam(file, param):
+    x = subprocess.check_output("samtools view -c " + param + file, shell=True)
+    return x
 
 
-def count_reads(file, param="" , paired_end=True):
-	if file.endswith("bam"):
-		return count_reads_bam(file , param)
-	if file.endswith("fastq") or file.endswith("fq"):
-		x = count_lines(file)
-		if (paired_end):
-			return int(x)/2
-		else:
-			return int(x)/4
-	if file.endswith("sam"):
-			return count_reads_bam(file , param)
-	return -1
+def count_reads(file, param="", paired_end=True):
+    if file.endswith("bam"):
+        return count_reads_bam(file, param)
+    if file.endswith("fastq") or file.endswith("fq"):
+        x = count_lines(file)
+        if (paired_end):
+            return int(x) / 2
+        else:
+            return int(x) / 4
+    if file.endswith("sam"):
+        return count_reads_bam(file, param)
+    return -1
 
 
 def sam_conversions(sam, depth=True):
-    cmd = "samtools view -bS " + sam + " > " + sam.replace(".sam",".bam") + "\n"
-    cmd += "samtools sort " + sam.replace(".sam",".bam") + " " + sam.replace(".sam" , "_sorted") + "\n"
-    cmd += "samtools index " + sam.replace(".sam" , "_sorted.bam") + "\n"
+    cmd = "samtools view -bS " + sam + " > " + sam.replace(".sam", ".bam") + "\n"
+    cmd += "samtools sort " + sam.replace(".sam", ".bam") + " " + sam.replace(".sam", "_sorted") + "\n"
+    cmd += "samtools index " + sam.replace(".sam", "_sorted.bam") + "\n"
     if depth:
-        cmd += "samtools depth " + sam.replace(".sam" , "_sorted.bam") + " > " + sam.replace(".sam" , "_sorted.depth") + "\n"
+        cmd += "samtools depth " + sam.replace(".sam", "_sorted.bam") + " > " + sam.replace(".sam",
+                                                                                            "_sorted.depth") + "\n"
     return cmd
 
+
+def bam_conversions(bam, depth=True):
+    cmd = "samtools view -h " + bam + " > " + bam.replace(".bam", ".sam") + "\n"
+    cmd += "samtools sort " + bam + " " + bam.replace(".bam", "_sorted") + "\n"
+    cmd += "samtools index " + bam.replace(".bam", "_sorted.bam") + "\n"
+    if depth:
+        cmd += "samtools depth " + bam.replace(".bam", "_sorted.bam") + " > " + bam.replace(".bam", "_sorted.depth") + "\n"
+    return cmd
