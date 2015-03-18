0cd0656fbe6ddffc3610c41e260b70a2ec4ea44c
André Rendeiro <afrendeiro@gmail.com>
2015-03-18 14:27:28 +0100

    structured code into a python package; close #1

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pypiper',
+    version='0.1',
+    description='A lightweight python toolkit for gluing together restartable, robust command line pipelines',
+    author='Nathan Sheffield, Johanna Klughammer, ',
+    author_email='nsheffield@cemm.oeaw.ac.at, jklughammer@cemm.oeaw.ac.at',
+    url='https://github.com/ComputationalEpigenetics/pypiper/',
+    packages=['pypiper'],
+)
