f53d0ed2583839c4eb1ef07c67e9fb606eb31cdd
NS <sheffien@users.noreply.github.com>
2015-08-25 08:42:33 +0200

    Update Email address

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(
     version='0.1',
     description='A lightweight python toolkit for gluing together restartable, robust command line pipelines',
     author='Nathan Sheffield, Johanna Klughammer, ',
-    author_email='nsheffield@cemm.oeaw.ac.at, jklughammer@cemm.oeaw.ac.at',
+    author_email='nathan@code.databio.org, jklughammer@cemm.oeaw.ac.at',
     url='https://github.com/ComputationalEpigenetics/pypiper/'
 )
