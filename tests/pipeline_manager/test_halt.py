14cb4283663dd6085b9191c022fe31c1621c0cb6
Vince Reuter <vince.reuter@gmail.com>
2017-12-08 17:30:10 -0500

    bit more detail to the basic halting test

--- tests/pipeline_manager/test_halt.py
+++ tests/pipeline_manager/test_halt.py
@@ -13,14 +13,16 @@ def test_halt_state(get_pipe_manager):
     """ Requesting a halt alters manager state. """
     pm = get_pipe_manager(name="test-pipe")
     assert pm.is_running
+    assert pm._active
     pm.halt()
-    assert not pm.is_running
     assert pm.halted
+    assert not pm.is_running
+    assert not pm._active
 
 
 
 def test_halt_file(get_pipe_manager):
-    """ Requesting a halt produces a particular flag file """
+    """ Requesting a halt produces a particular flag file. """
     pm = get_pipe_manager(name="TestPM")
     path_halt_file = pm.flag_file_path(PAUSE_FLAG)
     assert not os.path.isfile(path_halt_file)
