f44fd240ce2b66a7eef59fda5b15039791e0f758
Vince Reuter <vince.reuter@gmail.com>
2017-12-08 13:14:45 -0500

    implement first couple of constructor tests

--- tests/pipeline_manager/test_manager_constructor.py
+++ tests/pipeline_manager/test_manager_constructor.py
@@ -1,6 +1,8 @@
 """ Test effects of construction of a pipeline manager. """
 
+import argparse
 import pytest
+from tests.helpers import named_param
 
 
 __author__ = "Vince Reuter"
@@ -8,42 +10,69 @@ __email__ = "vreuter@virginia.edu"
 
 
 
-@pytest.mark.skip("Not implemented")
+def pytest_generate_tests(metafunc):
+    """ Dynamic test case generation for this module's test cases. """
+    if "spec_type" in metafunc.fixturenames:
+        metafunc.parametrize(
+                argnames="spec_type", argvalues=["cmdl", "kwargs"])
+
+
+
 class ManagerConstructorCheckpointSpecificationTests:
     """ Tests for manager's constructor's ability to parse and set
     checkpoint specifications, which can determine aspects of control flow. """
 
 
-    def test_no_checkpoint_specifications(self):
+    def test_no_checkpoint_specifications(self, get_pipe_manager):
         """ A manager may be constructed without any checkpoint provision. """
-        pass
+        get_pipe_manager(name="test-pipe")
 
 
-    def test_just_start(self):
+    @named_param("start_point", ["filter_reads", "align_reads"])
+    def test_just_start(self, get_pipe_manager, spec_type, start_point):
         """ Starting point may be set from command-line or ctor keyword. """
-        pass
+        spec_data = {"start_point": start_point}
+        if spec_type == "cmdl":
+            kwargs = {"args": argparse.Namespace(**spec_data)}
+        else:
+            kwargs = spec_data
+        pm = get_pipe_manager(name="start-test", **kwargs)
+        assert start_point == pm.start_point
 
-    
-    def test_just_stop(self):
+
+    @named_param("stop_type", ["stop_before", "stop_after"])
+    @named_param("stop_point", ["align_reads", "call_peaks"])
+    def test_just_stop(self, get_pipe_manager,
+                       spec_type, stop_type, stop_point):
         """ Particular stopping type is set correctly. """
-        pass
+        spec_data = {stop_type: stop_point}
+        if spec_type == "cmdl":
+            kwargs = {"args": argparse.Namespace(**spec_data)}
+        else:
+            kwargs = spec_data
+        pm = get_pipe_manager(name="stop-test", **kwargs)
+        assert stop_point == getattr(pm, stop_type)
 
 
-    def test_start_and_stop(self):
+    @pytest.mark.skip("Not implemented")
+    def test_start_and_stop(self, get_pipe_manager, spec_type):
         """ Specifying both start and stop works just fine. """
         pass
 
 
-    def test_both_stop_modes_is_prohibited(self):
+    @pytest.mark.skip("Not implemented")
+    def test_both_stop_modes_is_prohibited(self, get_pipe_manager, spec_type):
         """ Provision of both prospective and retrospective stop is bad. """
         pass
 
 
-    def test_complementary_specification_modes(self):
-        """ Command-line and keyword specifciations can harmonize. """
+    @pytest.mark.skip("Not implemented")
+    def test_complementary_specification_modes(self, get_pipe_manager):
+        """ Command-line and keyword specifications can harmonize. """
         pass
 
 
-    def test_command_line_beats_constructor_keyword(self):
+    @pytest.mark.skip("Not implemented")
+    def test_command_line_beats_constructor_keyword(self, get_pipe_manager):
         """ Command-line specification is favored over constructor keyword. """
         pass
