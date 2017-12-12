d078b1dfe67bb8a7e10267341114ee4a9bb9e976
Vince Reuter <vince.reuter@gmail.com>
2017-12-12 10:28:56 -0500

    pass the pipeline-unique checkpoint filenames tests

--- tests/pipeline/test_multi_pipeline_sample.py
+++ tests/pipeline/test_multi_pipeline_sample.py
@@ -27,16 +27,26 @@ def test_checkpoints_are_pipeline_unique(tmpdir):
     assert [] == list(fetch_checkpoint_files(call_peaks.manager))
 
     align_reads.run()
+    call_peaks.run()
 
     align_reads_expected = {checkpoint_filepath(s.name, align_reads)
                             for s in align_reads.stages()}
     call_peaks_expected = {checkpoint_filepath(s.name, call_peaks)
-                           for s in align_reads.stages()}
+                           for s in call_peaks.stages()}
     assert set() == (align_reads_expected & call_peaks_expected)
     expected_checkpoints = align_reads_expected | call_peaks_expected
     observed_checkpoints = set(fetch_checkpoint_files(align_reads)) | \
                            set(fetch_checkpoint_files(call_peaks))
-    assert expected_checkpoints == observed_checkpoints
+    try:
+        assert expected_checkpoints == observed_checkpoints
+    except AssertionError:
+        only_exp = expected_checkpoints - observed_checkpoints
+        exp_and_obs = expected_checkpoints & observed_checkpoints
+        only_obs = observed_checkpoints - expected_checkpoints
+        print("Only in expected:\n{}".format("\n".join(only_exp)))
+        print("Expected and observed:\n{}".format("\n".join(exp_and_obs)))
+        print("Only in observed:\n{}".format("\n".join(only_obs)))
+        raise
 
 
 
