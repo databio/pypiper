68b7a3219b3f4514fee00fdaeabe47827e3413fb
Vince Reuter <vince.reuter@gmail.com>
2017-12-12 10:34:16 -0500

    add some explanatory comments and ensure that full test suite still passes

--- tests/pipeline/test_multi_pipeline_sample.py
+++ tests/pipeline/test_multi_pipeline_sample.py
@@ -14,29 +14,45 @@ __email__ = "vreuter@virginia.edu"
 def test_checkpoints_are_pipeline_unique(tmpdir):
     """ Names of checkpoint files depend on both stage and pipeline. """
 
+    # Create two different pipelines.
     align_reads = get_read_aligner(tmpdir.strpath)
     call_peaks = get_peak_caller(tmpdir.strpath)
 
+    # Get the stage names associated with each pipeline.
     alignment_stage_names = set(map(lambda s: s.name, align_reads.stages()))
     peak_call_stage_names = set(map(lambda s: s.name, call_peaks.stages()))
 
+    # Check that we have one specific stage name shared between the pipelines.
     assert {"align_reads"} == alignment_stage_names & peak_call_stage_names
     assert align_reads.outfolder == call_peaks.outfolder
 
+    # We begin with no checkpoint files.
     assert [] == list(fetch_checkpoint_files(align_reads.manager))
     assert [] == list(fetch_checkpoint_files(call_peaks.manager))
 
+    # Run each pipeline.
     align_reads.run()
     call_peaks.run()
 
+    # We expect a different checkpoint file for each stage of each pipeline.
     align_reads_expected = {checkpoint_filepath(s.name, align_reads)
                             for s in align_reads.stages()}
     call_peaks_expected = {checkpoint_filepath(s.name, call_peaks)
                            for s in call_peaks.stages()}
+
+    # Pipeline names are unique here, and each checkpoint name includes
+    # pipeline name for disambiguation, so even a pair of pipelines with a
+    # nonempty stage name intersection has an empty checkpoint filenames
+    # intersection, so long as the pipeline names are unique.
     assert set() == (align_reads_expected & call_peaks_expected)
+
+    # When not setting start/stop parameters and beginning with no checkpoint
+    # files in place, each pipeline generates its full set of checkpoint files.
     expected_checkpoints = align_reads_expected | call_peaks_expected
     observed_checkpoints = set(fetch_checkpoint_files(align_reads)) | \
                            set(fetch_checkpoint_files(call_peaks))
+
+    # Verify satisfaction of expectation.
     try:
         assert expected_checkpoints == observed_checkpoints
     except AssertionError:
