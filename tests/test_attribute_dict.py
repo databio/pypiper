1641a5d8852de8e297862c14f8f2a385cb5a4cf6
Vince Reuter <vince.reuter@gmail.com>
2019-01-30 17:13:52 -0500

    brief test message

--- tests/test_attribute_dict.py
+++ tests/test_attribute_dict.py
@@ -7,6 +7,7 @@ from pypiper import AttributeDict as AD
 @pytest.mark.parametrize("base", ["random", "irrelevant", "arbitrary"])
 @pytest.mark.parametrize("protect", [False, True])
 def test_echo_respects_protected(base, protect):
+    """ Regardless of AttributeDict behavior, protected member isn't echoed. """
     ad = AD({}, default=True)
     if protect:
         with pytest.raises(AttributeError):
