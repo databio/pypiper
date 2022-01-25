b49a5afaa9e4449764d80ed7bcfee36468b7cd82
nsheff <nsheff@users.noreply.github.com>
2022-01-25 16:54:19 -0500

    Dicts are now ordered by default

--- pypiper/pipeline.py
+++ pypiper/pipeline.py
@@ -4,7 +4,7 @@ import abc
 import glob
 import os
 import sys
-from collections.abc import Iterable, Mapping, OrderedDict
+from collections.abc import Iterable, Mapping
 
 from .exceptions import \
     IllegalPipelineDefinitionError, IllegalPipelineExecutionError, \
@@ -392,9 +392,9 @@ def _is_unordered(collection):
     if not isinstance(collection, Iterable):
         raise TypeError("Non-iterable alleged collection: {}".
                         format(type(collection)))
-    return isinstance(collection, set) or \
-           (isinstance(collection, dict) and
-            not isinstance(collection, OrderedDict))
+
+
+    return isinstance(collection, set) or isinstance(collection, dict)
 
 
 
